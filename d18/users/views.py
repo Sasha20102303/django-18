25b172bfd199e62b5e2a439125a3c5e59da81ff8
init <ring@gmail.com>
2024-03-17 17:39:04 +1000

    23_work

--- d18/users/views.py
+++ d18/users/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from .models import User
 from .forms import UserLoginForm
 from django.auth import reverse
-
+from django.contrib import auth
 
 def login(request):
+    if request.method == 'POST':
+        pass
+    #дописать
     context = {
         'form':UserLoginForm(),
     }
